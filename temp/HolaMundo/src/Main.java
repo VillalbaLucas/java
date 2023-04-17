9518c73f0fd156153f77868d110b10c59504a708
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-04-16 21:40:44 -0300

    limpiando espacio de java

---
+++ temp/HolaMundo/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String saludo = scr.next();
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Hello world!" + saludo);
+    }
+}
