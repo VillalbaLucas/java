9b15d2fc6d4af155f05cba40cfd171da8cd3d93e
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-04-16 21:43:20 -0300

    delete temp

--- temp/HolaMundo/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        String saludo = scr.next();
-        Scanner scr = new Scanner(System.in);
-        System.out.println("Hello world!" + saludo);
-    }
-}
